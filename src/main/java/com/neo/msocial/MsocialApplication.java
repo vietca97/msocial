fb344cae52b08aedf4297ff3c4e69d62996918a8
vietlv <vietca97@gmail.com>
2021-12-27 10:43:51 +0700

    initial

---
+++ src/main/java/com/neo/msocial/MsocialApplication.java
@@ -0,0 +1,13 @@
+package com.neo.msocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsocialApplication.class, args);
+	}
+
+}
